07f4a1e328e036d5a9b6122d2d6e15a8f821cc06
nigelwhite <webdev@permaculture.org.uk>
2020-04-24 06:02:58 +0100

    try responsive images in index

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,9 +1,82 @@
 <template>
 	<Layout>
 		<template slot="hero-img">
-			<div class="has-bg-img"></div>
+			<!-- <div class="has-bg-img"></div> -->
+
+			<!-- // invalid regular expression flags
+			<v-img
+				:src="require('~/assets/images/hero/vista-mobile-768x161.jpg')"
+				:srcset="~/assets/images/hero/vista-mobile-768x161.jpg 768w, 
+				~/assets/images/hero/vista-tablet-1009x353.jpg 1009w, 
+				~/assets/images/hero/vista-desktop-1393x353.jpg 1393w, 
+				~/assets/images/hero/vista-fullhd-2500x353.jpg 2500w"
+				sizes="100vw"
+			></v-img> -->
+			<!-- // invalid regular expression flags
+			<v-img
+				:src="require('~/assets/images/hero/vista-mobile-768x161.jpg')"
+				:srcset="~/assets/images/hero/vista-mobile-768x161.jpg + ' 768w', 
+				~/assets/images/hero/vista-tablet-1009x353.jpg + ' 1009w', 
+				~/assets/images/hero/vista-desktop-1393x353.jpg + ' 1393w', 
+				~/assets/images/hero/vista-fullhd-2500x353.jpg + ' 2500w'"
+				sizes="100vw"
+			></v-img> -->
+			<!-- // invalid regular expression flags
+			<v-img
+				:src="require('~/assets/images/hero/vista-mobile-768x161.jpg')"
+				:srcset="`${~/assets/images/hero/vista-mobile-768x161.jpg} 768w`, 
+				`${~/assets/images/hero/vista-tablet-1009x353.jpg} 1009w`, 
+				`${~/assets/images/hero/vista-desktop-1393x353.jpg} 1393w`, 
+				`${~/assets/images/hero/vista-fullhd-2500x353.jpg} 2500w`"
+				sizes="100vw"
+			></v-img> -->
+			<!-- <v-img
+				:src="require('~/assets/images/hero/vista-mobile-768x161.jpg')"
+				:srcset="require('~/assets/images/hero/vista-mobile-768x161.jpg') 768w, 
+				require('~/assets/images/hero/vista-tablet-1009x353.jpg') 1009w, 
+				require('~/assets/images/hero/vista-desktop-1393x353.jpg') 1393w, 
+				require('~/assets/images/hero/vista-fullhd-2500x353.jpg') 2500w"
+				sizes="100vw"
+			></v-img> -->
+			<!-- <v-img :src="require('~/assets/images/hero/vista-mobile-768x161.jpg')">
+			</v-img> -->
+			<!-- <g-image src="~/assets/images/hero/vista-mobile-768x161.jpg"> </g-image> -->
+
+			<g-image
+				src="~/assets/images/hero/vista-fullhd-2500x353.jpg"
+				width="2500"
+			/>
+			<!-- works but always renders fullhd
+			<v-img
+				:src="require('~/assets/images/hero/vista-mobile-768x161.jpg')"
+				:srcset="
+					(require('~/assets/images/hero/vista-mobile-768x161.jpg') + ' 768w',
+					require('~/assets/images/hero/vista-tablet-1009x353.jpg') + ' 1009w',
+					require('~/assets/images/hero/vista-desktop-1393x353.jpg') + ' 1393w',
+					require('~/assets/images/hero/vista-fullhd-2500x353.jpg') + ' 2500w')
+				"
+				sizes="100vw"
+			></v-img> -->
+			<!-- <g-image
+				src="~/assets/images/hero/vista-mobile-768x161.jpg"
+				srcset="
+					~/assets/images/hero/vista-mobile-768x161.jpg 768w,
+					~/assets/images/hero/vista-tablet-1009x353.jpg 1009w,
+					~/assets/images/hero/vista-desktop-1393x353.jpg 1393w,
+					~/assets/images/hero/vista-fullhd-2500x353.jpg 2500w
+				"
+				sizes="100vw"
+			></g-image> -->
 		</template>
 		<h1>home page</h1>
+		<div class="mx-auto hannah-container">
+			<g-image src="~/assets/images/hannah.jpg" width="250" />
+		</div>
+		<!-- <v-img
+			:src="require('~/assets/images/hannah.jpg')"
+			max-width="250"
+			class="mx-auto"
+		></v-img> -->
 	</Layout>
 </template>
 
@@ -21,6 +94,7 @@ export default {
 	data() {
 		return {
 			cards,
+			path: '~/assets/images/hero/',
 		};
 	},
 };
@@ -41,4 +115,7 @@ export default {
   @media only screen and (min-width: 64em)
     height: 353px
     background: url("~@/assets/images/hero/vista-fullhd-2500x353.jpg") center center
+
+.hannah-container
+		width: 250px
 </style>
