344f02dbee8033c81effba7456e3976da65a4f19
nigelwhite <webdev@permaculture.org.uk>
2020-04-25 14:27:23 +0100

    pics on index

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -19,7 +19,7 @@
 		<template slot="title">{{ title }}</template>
 
 		<v-row>
-			<v-col class="col-12 col-md-8">
+			<v-col class="col-12 col-md-8 col-lg-9">
 				<div class="d-flex flex-wrap justify-center">
 					<div v-for="(card, index) in cards" v-show="card.show" :key="index">
 						<g-link :to="card.link">
@@ -29,15 +29,16 @@
 									class="ink-card mx-2 my-2"
 									max-width="250px"
 								>
-									<v-img
-										:src="require('~/assets/images/' + card.image)"
-										class="white--text align-end"
-										height="150px"
-									>
-										<v-card-title class="headline font-weight-bold">{{
-											card.title
-										}}</v-card-title>
-									</v-img>
+									<v-responsive :aspect-ratio="25 / 15">
+										<v-img
+											:src="require('~/assets/images/' + card.image)"
+											class="white--text align-end"
+										>
+											<v-card-title class="headline font-weight-bold">{{
+												card.title
+											}}</v-card-title>
+										</v-img>
+									</v-responsive>
 
 									<v-card-actions>
 										<v-btn color="orange" text>
@@ -80,11 +81,11 @@ export default {
 	-webkit-transition: all .2s ease-in-out
   transition: all .2s ease-in-out
 
-@media only screen and (min-width: 600px)
-	.ink-card
-		width: 175px
-	.ink-title
-		font-size: 2rem
-	.ink-subtitle
-		font-size: 1.5em
+// @media only screen and (min-width: 600px)
+// 	.ink-card
+// 		width: 175px
+// 	.ink-title
+// 		font-size: 2rem
+// 	.ink-subtitle
+// 		font-size: 1.5em
 </style>
