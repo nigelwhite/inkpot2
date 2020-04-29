bb38078c882556c3295248d1672793c2f9c39880
nigelwhite <webdev@permaculture.org.uk>
2020-04-29 13:30:22 +0100

    image in learn

--- src/pages/Learn.vue
+++ src/pages/Learn.vue
@@ -4,15 +4,22 @@
 			<div class="my-4 mb-lg-8">{{ title }}</div>
 		</template>
 		<v-row>
-			<v-col>
+			<v-col class="col-12 col-lg-5 col-xl-6">
 				<p>
 					All about learning Permaculture at The Inkpot
 				</p>
 				<p>Courses happen occasionally, etc</p>
 				<p>Working Wednesday, etc</p>
 			</v-col>
-			<v-col class="px-lg-12 text-center">
-				<v-img :src="img1" max-width="400" class="mx-auto"></v-img>
+			<v-col class="text-right">
+				<div>
+					<g-image
+						src="~/assets/images/veg.png"
+						width="900"
+						class="gimage"
+					></g-image>
+					<p class="text-right caption">Photo: Carla Moss</p>
+				</div>
 			</v-col>
 		</v-row>
 	</Layout>
@@ -28,34 +35,7 @@ export default {
 			title: 'Learn Permaculture',
 		};
 	},
-	props: {
-		img1: {
-			type: String,
-			default:
-				'https://via.placeholder.com/400x400.png?text=At+least+one+image',
-		},
-	},
 };
 </script>
 
-<style lang="sass" scoped>
-.outer-video-container
-	padding: 5%
-
-.video-container
-// min-width: 250px
-//   max-width: 444px
-
-.youtube-responsive-container
-  position: relative
-  padding-bottom: 56.25%
-  height: 0
-  overflow: hidden
-
-.youtube-responsive-container iframe
-  position: absolute
-  top: 0
-  left: 0
-  width: 100%
-  height: 100%
-</style>
+<style lang="sass" scoped></style>
