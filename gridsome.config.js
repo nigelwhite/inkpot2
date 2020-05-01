a3e229bc1099a817bdbd96c34b6c6e74a070a740
nigelwhite <webdev@permaculture.org.uk>
2020-05-01 15:39:55 +0100

    add plugin details to config.js

--- gridsome.config.js
+++ gridsome.config.js
@@ -25,5 +25,14 @@ module.exports = {
 			addStyleResource(config.module.rule('sass').oneOf(type));
 		});
 	},
-	plugins: [],
+	plugins: [
+		{
+			use: 'gridsome-source-google-sheets',
+			options: {
+				sheetId: 'GOOGLE_SHEET_ID',
+				apiKey: 'GOOGLE_API_KEY',
+				// type: 'TYPE_NAME', //Optional - default is googleSheet. Used for graphql queries.
+			},
+		},
+	],
 };
