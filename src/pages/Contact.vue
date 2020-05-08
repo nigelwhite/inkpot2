5b4f69d33439431dfcafe79b959c985525b1c6e4
nigelwhite <webdev@permaculture.org.uk>
2020-05-08 08:38:06 +0100

    start contact form

--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -4,12 +4,51 @@
 			<div class="my-4 mb-lg-8">{{ title }}</div>
 		</template>
 		<v-row>
-			<v-col class="col-3 text-center">
+			<v-col>
+				<h2>Please contact Hannah by sending a message</h2>
+				<div class="message-form">
+					<v-form
+						ref="form"
+						v-model="valid"
+						lazy-validation
+						class="mt-8 mx-lg-12"
+					>
+						<v-text-field
+							v-model="name"
+							:counter="20"
+							:rules="nameRules"
+							label="Name"
+							required
+						></v-text-field>
+
+						<v-text-field
+							v-model="email"
+							:rules="emailRules"
+							label="E-mail"
+							required
+						></v-text-field>
+
+						<v-text-field label="Message"></v-text-field>
+
+						<v-btn
+							:disabled="!valid"
+							color="success"
+							class="mr-4"
+							@click="validate"
+						>
+							Send
+						</v-btn>
+
+						<v-btn color="error" class="mr-4" @click="reset">
+							Clear
+						</v-btn>
+					</v-form>
+				</div>
+			</v-col>
+			<v-col class="text-center mt-lg-12">
 				<g-link to="/location/">
 					<v-btn class="secondary"> Map and address</v-btn>
-				</g-link> </v-col
-			><v-col>
-				<v-img :src="img2" max-width="600"></v-img>
+				</g-link>
 			</v-col>
 		</v-row>
 	</Layout>
@@ -20,19 +59,36 @@ export default {
 	metaInfo: {
 		title: 'Inkpot | Contact',
 	},
-	data() {
-		return {
-			title: 'Contact The Inkpot',
-		};
-	},
-	props: {
-		img2: {
-			type: String,
-			default:
-				'https://via.placeholder.com/600x400.png?text=Contact+form+for+messages',
+
+	data: () => ({
+		valid: true,
+		name: '',
+		nameRules: [
+			(v) => !!v || 'Name is required',
+			(v) => (v && v.length <= 10) || 'Name must be less than 10 characters',
+		],
+		email: '',
+		emailRules: [
+			(v) => !!v || 'E-mail is required',
+			(v) => /.+@.+\..+/.test(v) || 'E-mail must be valid',
+		],
+	}),
+
+	methods: {
+		validate() {
+			this.$refs.form.validate();
+		},
+		reset() {
+			this.$refs.form.reset();
 		},
 	},
+	// return {
+	// 	title: 'Contact The Inkpot',
+	// };
 };
 </script>
 
-<style lang="sass" scoped></style>
+<style lang="sass" scoped>
+.message-form
+	max-width: 900px
+</style>
