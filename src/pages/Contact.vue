bc1396942f34d39867b83a6d91208ccd52c66b76
nigelwhite <webdev@permaculture.org.uk>
2020-05-08 08:05:56 +0100

    location page

--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -4,20 +4,12 @@
 			<div class="my-4 mb-lg-8">{{ title }}</div>
 		</template>
 		<v-row>
-			<v-col>
-				<p>
-					Address
-				</p>
-				<p>Travel directions</p>
-				<p>etc</p>
-			</v-col>
-		</v-row>
-		<v-row>
-			<v-col>
+			<v-col class="col-3 text-center">
+				<g-link to="/location/">
+					<v-btn class="secondary"> Map and address</v-btn>
+				</g-link> </v-col
+			><v-col>
 				<v-img :src="img2" max-width="600"></v-img>
-			</v-col>
-			<v-col class="px-lg-12">
-				<v-img :src="img1" max-width="400" class="mx-auto"></v-img>
 			</v-col>
 		</v-row>
 	</Layout>
@@ -34,10 +26,6 @@ export default {
 		};
 	},
 	props: {
-		img1: {
-			type: String,
-			default: 'https://via.placeholder.com/400x400.png?text=Google+map',
-		},
 		img2: {
 			type: String,
 			default:
@@ -47,24 +35,4 @@ export default {
 };
 </script>
 
-<style lang="sass" scoped>
-.outer-video-container
-	padding: 5%
-
-.video-container
-// min-width: 250px
-//   max-width: 444px
-
-.youtube-responsive-container
-  position: relative
-  padding-bottom: 56.25%
-  height: 0
-  overflow: hidden
-
-.youtube-responsive-container iframe
-  position: absolute
-  top: 0
-  left: 0
-  width: 100%
-  height: 100%
-</style>
+<style lang="sass" scoped></style>
---
+++ src/pages/Location.vue
@@ -0,0 +1,64 @@
+<template>
+	<Layout>
+		<template slot="title">
+			<div class="my-4 mb-lg-8">{{ title }}</div>
+		</template>
+		<v-row>
+			<v-col>
+				<div class="ml-lg-8">
+					<p>
+						Hannah Thorogood<br />The Inkpot<br />Highgate Farm<br />Gorse
+						Drove<br />Scredington<br />Sleaford<br />NG34 0AH
+					</p>
+					<p>
+						To send a message
+						<g-link to="/contact/">
+							<v-btn small class="secondary"> Contact us</v-btn>
+						</g-link>
+					</p>
+					<p>
+						If you query is urgent please feel free to text Hannah on 07801
+						814086
+					</p>
+					<p>Travel directions</p>
+				</div>
+			</v-col>
+
+			<v-col>
+				<div>
+					<iframe
+						width="600"
+						height="494"
+						frameborder="0"
+						scrolling="no"
+						marginheight="0"
+						marginwidth="0"
+						src="https://www.openstreetmap.org/export/embed.html?bbox=-0.38018703460693365%2C52.93231852759099%2C-0.3235387802124024%2C52.95536039375423&amp;layer=mapnik&amp;marker=52.94384099466375%2C-0.35186290740966797"
+						style="border: 1px solid black"
+					></iframe
+					><br /><small
+						><a
+							href="https://www.openstreetmap.org/?mlat=52.9438&amp;mlon=-0.3519#map=15/52.9438/-0.3519"
+							>View Larger Map</a
+						></small
+					>
+				</div>
+			</v-col>
+		</v-row>
+	</Layout>
+</template>
+
+<script>
+export default {
+	metaInfo: {
+		title: 'Inkpot | Location',
+	},
+	data() {
+		return {
+			title: 'Location',
+		};
+	},
+};
+</script>
+
+<style lang="sass" scoped></style>
