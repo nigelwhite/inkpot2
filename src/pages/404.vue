a2d226823eb99b8d8f691f97bf173076734104a5
nigelwhite <webdev@permaculture.org.uk>
2020-05-13 15:19:11 +0100

    layout 404

--- src/pages/404.vue
+++ src/pages/404.vue
@@ -1,5 +1,12 @@
 <template>
 	<Layout>
-		Sorry, not here
+		<div class="d-flex justify-center align-center text-center announce">
+			<h1>Sorry<br />the page you asked for is not on the site</h1>
+		</div>
 	</Layout>
 </template>
+
+<style lang="sass" scoped>
+.announce
+	height: 500px
+</style>
