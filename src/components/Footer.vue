f2020fe0c4f5c23ad07810d9e6fadebaa83758f6
nigelwhite <webdev@permaculture.org.uk>
2020-05-29 17:35:58 +0100

    footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,22 +1,41 @@
 <template>
-	<v-footer height="75">
+	<v-footer>
 		<v-row justify="center" align="center">
-			<div>
-				<a
-					href="https://www.permaculture.org.uk/land"
-					target="_blank"
-					class="d-flex flex-column flex-md-row"
-				>
-					<g-image
-						src="~/assets/images/land-logo.png"
-						alt="Inkpot Logo"
-						width="150"
-						class="my-4"
-					/>
-					<p class="align-self-center ml-6 title">
-						Permaculture Land Demonstration Network
-					</p>
-				</a>
+			<div class="d-flex flex-column flex-md-row my-6">
+				<div class="mr-md-12">
+					<a
+						href="https://www.permaculture.org.uk/land"
+						target="_blank"
+						class="d-flex flex-column"
+					>
+						<g-image
+							src="~/assets/images/logos/land-logo.png"
+							alt="LAND Demonstration Network logo"
+							width="200"
+							class="align-self-center"
+						/>
+						<p class="title align-self-center">
+							Permaculture Land Demonstration Network
+						</p>
+					</a>
+				</div>
+				<div class="ml-md-12 ">
+					<a
+						href="https://greattasteawards.co.uk/"
+						target="_blank"
+						class="d-flex flex-column"
+					>
+						<g-image
+							src="~/assets/images/logos/GTproducer250x250.png"
+							alt="Great Taste Producer logo"
+							width="80"
+							class="align-self-center"
+						/>
+						<p class="title align-self-center">
+							Great Taste Producer
+						</p>
+					</a>
+				</div>
 			</div>
 		</v-row>
 	</v-footer>
