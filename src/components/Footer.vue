475d67baac96ca49afa7100be0c23b87e3fb2554
nigelwhite <webdev@permaculture.org.uk>
2020-05-29 16:22:40 +0100

    land logo in footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,35 +1,29 @@
 <template>
 	<v-footer height="75">
 		<v-row justify="center" align="center">
-			<div class="d-flex flex-column flex-md-row">
-				<v-btn v-for="link in links" :key="link.name" text rounded class="mr-4">
-					<g-link :to="link.url" class="title">
-						{{ link.name }}
-					</g-link>
-				</v-btn>
+			<div>
+				<a
+					href="https://www.permaculture.org.uk/land"
+					target="_blank"
+					class="d-flex flex-column flex-md-row"
+				>
+					<g-image
+						src="~/assets/images/land-logo.png"
+						alt="Inkpot Logo"
+						width="150"
+						class="my-4"
+					/>
+					<p class="align-self-center ml-6 title">
+						Permaculture Land Demonstration Network
+					</p>
+				</a>
 			</div>
 		</v-row>
 	</v-footer>
 </template>
 
 <script>
-export default {
-	data() {
-		return {
-			links: [
-				{
-					name: 'About',
-					url: '/about/',
-				},
-
-				{
-					name: 'Contact',
-					url: '/contact/',
-				},
-			],
-		};
-	},
-};
+export default {};
 </script>
 
 <style scoped></style>
